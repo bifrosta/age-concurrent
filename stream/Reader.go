669c850a202c2dc7724644a4a018b0c77407d89f
Klaus Post <klauspost@gmail.com>
2025-02-08 15:13:29 +0100

    Remove allocs and handle last buffer.

--- stream/Reader.go
+++ stream/Reader.go
@@ -31,12 +31,28 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 	decrypted := make(chan chan []byte, concurrent)
 	done := make(chan error, 1)
 
+	// Reusable output buffer, one extra for an active job
+	// Another that is being written
+	reBuf := make(chan []byte, concurrent+2)
+	// Reusable jobs.
+	// Add one extra so a job can be prepared while 'concurrent' are being decoded.
+	reJob := make(chan *job, concurrent+1)
+	for i := 0; i < concurrent+1; i++ {
+		reBuf <- make([]byte, encChunkSize)
+		reJob <- &job{out: make(chan []byte, 1), in: make([]byte, encChunkSize)}
+	}
+	reBuf <- make([]byte, encChunkSize)
+
 	r := &Reader{
 		reader: reader,
 	}
-
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		defer func() { close(todo) }()
+		defer func() {
+			close(todo)
+			wg.Done()
+		}()
 
 		var err error
 		var n int
@@ -44,12 +60,12 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 		last := false
 
 		for !last {
-			buffer := make([]byte, encChunkSize)
+			j := <-reJob
+			buffer := j.in[:encChunkSize]
 			n, err = io.ReadFull(src, buffer)
 			switch {
 			case err == io.EOF:
 				return
-
 			case err == io.ErrUnexpectedEOF:
 				// The last chunk can be short, but not empty unless it's the first and
 				// only chunk.
@@ -69,13 +85,9 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 				return
 			}
 
-			j := &job{
-				in:   buffer[:n],
-				last: last,
-				out:  make(chan []byte, 1),
-			}
-
-			copy(j.nonce[:], nonce[:])
+			j.in = buffer[:n]
+			j.last = last
+			j.nonce = nonce
 
 			todo <- j
 			decrypted <- j.out
@@ -84,26 +96,31 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 		}
 	}()
 
-	var wg sync.WaitGroup
-
 	wg.Add(concurrent)
 
 	for i := 0; i < concurrent; i++ {
 		go func() {
 			defer wg.Done()
-
+			// Track if we have seen a last nonce
+			hasSeenLast := false
 			for j := range todo {
+				if hasSeenLast && len(j.in) > 0 {
+					writer.CloseWithError(errors.New("unexpected data after last block"))
+					return
+				}
 				if j.last {
 					setLastChunkFlag(&j.nonce)
+					hasSeenLast = true
 				}
+				dst := <-reBuf
 
-				plaintext, err := a.Open(j.in[:0], j.nonce[:], j.in, nil)
+				plaintext, err := a.Open(dst[:0], j.nonce[:], j.in, nil)
 				if err != nil {
 					if !j.last {
 						// Check if this was a full-length final chunk.
-						j.last = true
+						hasSeenLast = true
 						setLastChunkFlag(&j.nonce)
-						plaintext, err = a.Open(j.in[:0], j.nonce[:], j.in, nil)
+						plaintext, err = a.Open(dst[:0], j.nonce[:], j.in, nil)
 					}
 
 					if err != nil {
@@ -112,8 +129,8 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 						return
 					}
 				}
-
 				j.out <- plaintext
+				reJob <- j
 			}
 		}()
 	}
@@ -122,7 +139,9 @@ func NewReader(a cipher.AEAD, src io.Reader, concurrent int) *Reader {
 		for d := range decrypted {
 			buffer := <-d
 
+			// TODO: Handle write errors
 			_, _ = writer.Write(buffer)
+			reBuf <- buffer
 		}
 
 		done <- writer.Close()
